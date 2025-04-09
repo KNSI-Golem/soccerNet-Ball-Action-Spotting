2e8f410f80b6f25b34ef7914a04ac4b42a9b1ae8
Filip Kobus <kobusfilip@outlook.com>
2025-04-09 11:00:43 +0000

    Improved download dataset

---
+++ src/download_dataset.py
@@ -0,0 +1,54 @@
+"""
+Code to download or generate data
+"""
+
+from pathlib import Path
+from src.config import PROCESSED_DATA_DIR, RAW_DATA_DIR
+from huggingface_hub import snapshot_download
+
+import pyzipper
+from dotenv import load_dotenv
+import os
+
+def downloadDataset(directory):
+    snapshot_download(repo_id="SoccerNet/SN-BAS-2025",
+                    repo_type="dataset", revision="main",
+                    local_dir=directory)
+
+def unzipFile(input_path, password, output_path):   
+    with pyzipper.AESZipFile(input_path) as zf:
+            zf.pwd = password
+            zf.extractall(output_path)
+
+def unzipDirectory(directory, password):
+    for filename in os.listdir(directory):
+        if filename.endswith('.zip'):
+            zip_path = os.path.join(directory, filename)
+            print(f"\n🔓 Unzipping: {zip_path}")
+
+            try:
+                output_path = directory / 'soccernetball'
+                unzipFile(zip_path, password, output_path)
+                print(f"✅ Extracted to: {output_path}")
+                
+                os.remove(zip_path)
+                print(f"🗑️ Deleted zip file: {zip_path}")
+            
+            except RuntimeError as e:
+                print(f"❌ Failed to unzip {filename}: {e}")
+
+def main(
+    input_path: Path = RAW_DATA_DIR,
+    output_path: Path = PROCESSED_DATA_DIR
+    
+):
+    load_dotenv()
+    password = os.getenv("DATA_PASSWD").encode()
+    extra_files = input_path / "ExtraLabelsActionSpotting500games"
+    downloadDataset(input_path)
+    unzipDirectory(input_path, password)
+    unzipDirectory(extra_files, password)
+
+
+if __name__ == "__main__":
+    main()
