85d5cae5ba9739f0994364adab2012ba7218845b
Filip Kobus <plgkobusf112@login01.athena.cyfronet.pl>
2025-04-10 20:12:21 +0200

    Updated config file for handling data download

--- src/config.py
+++ src/config.py
@@ -11,6 +11,7 @@ load_dotenv()
 
 # Paths
 PROJ_ROOT = Path(__file__).resolve().parents[1]
+DISK_ROOT = Path("/path/to/disk/root")
 
 DATA_DIR = PROJ_ROOT / "data"
 RAW_DATA_DIR = DATA_DIR / "raw"
@@ -22,3 +23,9 @@ MODELS_DIR = PROJ_ROOT / "models"
 
 REPORTS_DIR = PROJ_ROOT / "reports"
 FIGURES_DIR = REPORTS_DIR / "figures"
+
+SNB_DATA_DIR = DISK_ROOT / "soccernetball"
+SN_DATA_DIR = DISK_ROOT / "soccernet"
+
+SNB_FRAMES_DIR = DISK_ROOT / "frames_snb"
+SN_FRAMES_DIR = DISK_ROOT / "frames_sn"
